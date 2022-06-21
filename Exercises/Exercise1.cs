19891f414ab4330772e4def529bf306cbe3423bf
KripaNB <62209438+KripaNB@users.noreply.github.com>
2022-06-21 17:00:47 +0530

    Exercise1

---
+++ Exercises/Exercise1.cs
@@ -0,0 +1,32 @@
+﻿/* Write a program and ask the user to enter a number. The number should be between 1 and 10. 
+ * If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid" */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            int number;
+            Console.WriteLine("Enter a number:");
+            value = Console.ReadLine();
+            number = Convert.ToInt32(value);
+            if(number >= 1 && number <=10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
