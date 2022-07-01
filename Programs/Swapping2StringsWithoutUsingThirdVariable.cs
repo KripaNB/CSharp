1cae7ef0ee16b80c84582e94323d351efa098507
KripaNB <62209438+KripaNB@users.noreply.github.com>
2022-07-01 15:00:40 +0530

    Swapping 2 Strings Without Using Third Variable

---
+++ Programs/Swapping2StringsWithoutUsingThirdVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Swapping2StringsWithoutUsingThirdVariable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Enter another string:");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("The numbers before swapping : name1 = " + name1 + "; name2 = " + name2);
+            name1 = name1 + name2;
+            name2 = name1.Substring(0, name1.Length - name2.Length);
+            name1 = name1.Substring(name2.Length);
+            Console.WriteLine("The numbers after swapping are : name1 = " + name1 + "; name2 = " + name2);
+        }
+    }
+}
