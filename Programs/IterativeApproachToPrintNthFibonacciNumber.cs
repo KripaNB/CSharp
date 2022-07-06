7b1caf5dad76c804418bbf950bfe3b94e40515ad
KripaNB <62209438+KripaNB@users.noreply.github.com>
2022-07-06 09:20:35 +0530

    Fibonacci Series Programs

---
+++ Programs/IterativeApproachToPrintNthFibonacciNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterativeApproachToPrintNthFibonacciNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Nth number of the Fibonacci Series to be printed:");
+            int NthNumber = Convert.ToInt32(Console.ReadLine());
+            NthNumber = NthNumber - 1;
+            Console.WriteLine(NthFibonacciNumber(NthNumber));
+        }
+
+        public static int NthFibonacciNumber (int number)
+        {
+            int firstNumber = 0, secondNumber = 1, nextNumber;
+            if (number == 0)
+                return firstNumber;
+            for (int i = 2; i <= number; i++)
+            {
+                nextNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+            }
+            return secondNumber;
+        }
+    }
+}
