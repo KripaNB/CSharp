53d619c56904dd4a1f208e19ff5404d4bb85d82b
KripaNB <62209438+KripaNB@users.noreply.github.com>
2022-07-07 09:01:11 +0530

    Prime Numbers Programs

---
+++ Programs/GivenNumberIsPrimeOrNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GivenNumberIsPrimeOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for ( int i = 2; i < number/2; i++ )
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }                    
+
+            }
+            if (isPrime)
+                Console.WriteLine("The number is Prime");
+            else
+                Console.WriteLine("The number is not Prime");
+        }
+    }
+}
---
+++ Programs/PrintPrimeNumbersBetweenARange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPrimeNumbersBetweenARange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the start number:");
+            int startNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the end number:");
+            int endNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0 && i != 1)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
