cac72852a0fc421f73ddf8eef6b360f1ed09c577
KripaNB <62209438+KripaNB@users.noreply.github.com>
2023-09-26 17:21:56 +0530

    Basic Programs

---
+++ Programs/Duplicate Characters/DuplicateCharacters.cs
@@ -0,0 +1,30 @@
+﻿/*
+* Output:
+  Enter a string : Google
+  Gogle   
+*/
+
+using System;
+using System.Linq;
+
+namespace DuplicateCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+            string inputString = Console.ReadLine();
+            string resultString = string.Empty;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!resultString.Contains(inputString[i]))
+                {
+                    resultString = resultString + inputString[i];
+                }
+            }
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
